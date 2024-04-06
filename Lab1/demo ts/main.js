a3a29ae9b3608475168b719d0a398f76cbb54740
karim Mohamed <karimdesouki147@gmail.com>
2024-04-06 16:10:07 +0200

    lab 2 uploaded

--- Lab1/demo ts/main.js
+++ Lab1/demo ts/main.js
@@ -1,3 +1,43 @@
+//  class Account
+class Account {
+    constructor(Acc_no = 1, Balance = 10000) {
+        this.Acc_no = Acc_no;
+        this.Balance = Balance;
+    }
+    debitAmount() {
+        return (`Here is your debitAmount`);
+    }
+    creditAmount() {
+        return (`Here is your creditAmount`);
+    }
+    getBalance() {
+        return (`Here is your getBalance`);
+    }
+}
+// Saving_account class 
+class Saving_Account extends Account {
+    constructor(Acc_no = 1, Balance = 10000) { super(Acc_no, Balance); } //gets data from the main class
+    addCustomer() {
+        return ("Customer added successfully");
+    }
+    ;
+    removeCustomer() {
+        return ("Customer removed successfully");
+    }
+    ;
+}
+// Current_Account class 
+class Current_Account extends Account {
+    constructor(Acc_no = 1, Balance = 10000) { super(Acc_no, Balance); } //gets data from the main class
+    addCustomer() {
+        return ("Customer added successfully");
+    }
+    ;
+    removeCustomer() {
+        return ("Customer removed successfully");
+    }
+    ;
+}
 //#region class
 class Person {
     constructor(name = "person name", age = 0) {
@@ -44,9 +84,11 @@ class person2 {
     }
 }
 //#endregion
+//#region testing
 let per1 = new Person(); //Default constructor
 let per2 = new Person("karim mohamed", 20); //parametrized constructor
 let per3 = new Person("ahmad galaal", 30); //parametrized constructor 
 let emp1 = new Employee();
 let emp2 = new Employee("Yousef", 30);
 let emp3 = new Employee("AmrKhaled", 30);
+//#endregion
